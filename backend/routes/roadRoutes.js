c06f3957998444859e85cd3109ab8bc37d1f8d01
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-08-02 17:08:34 +0530

    Add bonus task

--- backend/routes/roadRoutes.js
+++ backend/routes/roadRoutes.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const roadController = require('../controllers/roadController');
 
-router.post('/roads',roadController.createRoad);
+router.post('/roads', roadController.createRoad);
+
+router.get('/roads/:id/traffic-condition', roadController.getTrafficCondition);
+
+router.get('/report/traffic', roadController.generateTrafficReport);
+
+router.get('/report/traffic/updates', roadController.analyzeTrafficUpdates);
 
 module.exports = router;
